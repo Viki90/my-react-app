daee2bbc2004cbd0f7f025b7715b7227b3de5fd8
viktorija-verebes <viki.verebes90@gmail.com>
2023-03-17 21:27:34 +0100

    navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetup';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div className="App">
-      
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />}  exact/>
+        <Route path='/new-meetup' element={<NewMeetupPage />} />
+        <Route path='/favorites' element={<FavoritesPage />} />
+      </Routes>
     </div>
   );
 }
