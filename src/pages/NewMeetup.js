a41a364b49026261367cb4b89d6def53750f8f80
viktorija-verebes <viki.verebes90@gmail.com>
2023-03-24 20:38:54 +0100

    useNavigate hook

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,8 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+
+    const history = useNavigate();
+
     function addMeetupHandler(meetupData) {
-        fetch('https://react-app-2c936-default-rtdb.firebaseio.com/meetups');
+        fetch('https://react-app-2c936-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': "application/json"
+                }
+
+            }
+        ).then(() => {
+            history('/', { replace: true });
+        });
     }
 
     return (
