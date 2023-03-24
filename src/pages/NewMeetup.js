6367359c77df26c256ffc5071db1758a1d143646
viktorija-verebes <viki.verebes90@gmail.com>
2023-03-24 20:14:18 +0100

    firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,14 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-app-2c936-default-rtdb.firebaseio.com/meetups');
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm></NewMeetupForm>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     );
 }
