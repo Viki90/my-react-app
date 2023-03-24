77d04e15f15a387d0a6c7ee7fba4ab154858e3bd
viktorija-verebes <viki.verebes90@gmail.com>
2023-03-24 17:18:09 +0100

    added card and layout components

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import AllMeetupsPage from './pages/AllMeetup';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
 import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />}  exact/>
         <Route path='/new-meetup' element={<NewMeetupPage />} />
         <Route path='/favorites' element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
